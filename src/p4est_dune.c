9012e6f8f44fe1acc920ea8c68c50c7e5af67367
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2023-11-29 19:40:22 +0100

    DUNE: number corners for uniform refinement

--- src/p4est_dune.c
+++ src/p4est_dune.c
@@ -32,6 +32,14 @@
 #include <p8est_lnodes.h>
 #endif
 
+/* map element corner to element node index */
+static const int    corner_enode[P4EST_CHILDREN] = {
+  0, 4, 20, 24
+#ifdef P4_TO_P8
+    , 100, 104, 120, 124
+#endif
+};
+
 /* set default parameters */
 void
 p4est_dune_numbers_params_default (p4est_dune_numbers_params_t * params)
@@ -42,19 +50,87 @@ p4est_dune_numbers_params_default (p4est_dune_numbers_params_t * params)
   params->ctype = P4EST_CONNECT_FULL;
 }
 
+static              p4est_gloidx_t
+lni_to_gni (p4est_lnodes_t * ln, p4est_locidx_t lni)
+{
+  p4est_locidx_t      oco;
+
+  P4EST_ASSERT (0 <= lni);
+
+  if (lni < (oco = ln->owned_count)) {
+    return ln->global_offset + lni;
+  }
+  else {
+    P4EST_ASSERT (lni < ln->num_local_nodes);
+    return ln->nonlocal_nodes[lni - oco];
+  }
+}
+
 /* Populate number arrays for real.  This function does all the work. */
 static void
-generate_numbers (p4est_dune_numbers_t * dn, p4est_lnodes_t *ln)
+generate_numbers (p4est_dune_numbers_t * dn, p4est_lnodes_t * ln)
 {
+  p4est_lnodes_code_t fc;
   p4est_locidx_t      lne, e;
+  p4est_locidx_t      lni;
+  p4est_locidx_t     *eno;
+  p4est_gloidx_t     *geco;
+  int                 c;
+  int                 do_c, do_f;
+#ifdef P4_TO_P8
+  int                 do_e;
+#endif
 
+  /* basic verification of input parameters */
   P4EST_ASSERT (dn != NULL);
   P4EST_ASSERT (ln != NULL);
   P4EST_ASSERT (ln->degree == 4);
 
-  lne = ln->num_local_elements;
-  for (e = 0; e < lne; ++e) {
+  /* examine which codimensions are handled */
+  do_c = dn->element_corners != NULL;
+#ifdef P4_TO_P8
+  do_e = 0;
+#endif
+  do_f = 0;
+  if (!do_c &&
+#ifdef P4_TO_P8
+      !do_e &&
+#endif
+      !do_f) {
+    /* there is absolutely nothing to do */
+    return;
+  }
 
+  /* prepare loop over elements to assign globally unique node indices */
+  lne = ln->num_local_elements;
+  eno = ln->element_nodes;
+  for (e = 0; e < lne; ++e) {
+    /* retrieve pointers to place output */
+    if (do_c) {
+      geco = (p4est_gloidx_t *)
+        sc_array_index (dn->element_corners, e * P4EST_CHILDREN);
+    }
+
+    /* check for hanging status of the element */
+    if (!(fc = ln->face_code[e])) {
+      /* the element is not the small neighbor to any other */
+      if (do_c) {
+        for (c = 0; c < P4EST_CHILDREN; ++c) {
+          /* retrieve local node number */
+          lni = eno[corner_enode[c]];
+          geco[c] = lni_to_gni (ln, lni);
+        }
+      }
+      if (do_f) {
+        /* face nodes not yet implemented */
+        SC_ABORT_NOT_REACHED ();
+      }
+    }
+    else {
+      /* hanging elements are not yet implemented */
+      SC_ABORT_NOT_REACHED ();
+    }
+    eno += P4EST_CHILDREN;
   }
 }
 
@@ -87,8 +163,9 @@ p4est_dune_numbers_new (p4est_t * p4est, p4est_ghost_t * ghost,
   }
 
   /* generate temporary local ghost (if needed) and node numbering */
-  ln = p4est_lnodes_new (p4est, ghost == NULL ?
-    (gh = p4est_ghost_new_local (p4est, pa->ctype)) : (gh = NULL, ghost), 4);
+  ln = p4est_lnodes_new
+    (p4est, ghost == NULL ?
+     (gh = p4est_ghost_new_local (p4est, pa->ctype)) : (gh = NULL, ghost), 4);
   if (gh != NULL) {
     /* this was a local lnodes without parallel neighbors */
     p4est_ghost_destroy (gh);
