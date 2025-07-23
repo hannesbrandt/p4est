aed49742ba7feeb69f699a9ca673e35ee5e3bf80
Hannes Brandt <brandt@ins.uni-bonn.de>
2025-07-23 16:27:05 +0200

    static: do not mark inlines as static in p{4,8}est_connectivity.h

--- src/p4est_connectivity.h
+++ src/p4est_connectivity.h
@@ -982,7 +982,7 @@ int                 p4est_connectivity_is_equivalent (p4est_connectivity_t *
 
 /** Return a pointer to a p4est_corner_transform_t array element. */
 /*@unused@*/
-static inline p4est_corner_transform_t *
+inline p4est_corner_transform_t *
 p4est_corner_array_index (sc_array_t * array, size_t it)
 {
   P4EST_ASSERT (array->elem_size == sizeof (p4est_corner_transform_t));
