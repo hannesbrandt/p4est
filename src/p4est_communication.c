a559645a917a439dfd1ae53bb07dd541d31e6ec7
Hannes Brandt <brandt@ins.uni-bonn.de>
2026-04-10 19:28:06 +0200

    Feature transfer_search: use transfer_search_{begin,end} in transfer_search

--- src/p4est_communication.c
+++ src/p4est_communication.c
@@ -2272,50 +2272,6 @@ static int
 static int
  p4est_transfer_search_internal_end (p4est_transfer_internal_t *internal);
 
-int
-p4est_transfer_search (p4est_t *p4est, p4est_queries_context_t *c,
-                       p4est_intersect_t intersect_fn)
-{
-  int                 err;
-
-  /* Init internal context */
-  p4est_transfer_internal_t internal;
-  memset (&internal, 0, sizeof (internal));
-
-  /* Assign context information */
-  P4EST_ASSERT (p4est_queries_context_is_valid (c));
-  internal.c = c;
-  internal.intersect_fn = intersect_fn;
-  internal.max_weight = -1;     /* marks that we do not work with weights */
-  internal.query_weight_fn = NULL;
-  internal.p4est = p4est;
-  internal.mpicomm = p4est->mpicomm;
-  internal.save_outside = 0;
-
-  /* These variables are not used because internal.p4est is not NULL */
-  internal.gfp = NULL;
-  internal.nmemb = -1;
-  internal.num_trees = -1;
-
-  /* Store p4est user pointer inside internal context */
-  internal.user_pointer = p4est->user_pointer;
-  /* Temporarily replace user pointer with internal context */
-  p4est->user_pointer = &internal;
-
-  /* Call internal transfer search */
-  err = p4est_transfer_search_internal_begin (&internal);
-  if (err == 0) {
-    /* Only call end if begin exits without an error */
-    err = p4est_transfer_search_internal_end (&internal);
-  }
-
-  /* Restore user pointer */
-  p4est->user_pointer = internal.user_pointer;
-
-  /* Return 0 if transfer was successful */
-  return err;
-}
-
 p4est_transfer_internal_t *
 p4est_transfer_search_begin (p4est_t * p4est,
                              p4est_queries_context_t * c,
@@ -2371,6 +2327,18 @@ p4est_transfer_search_end (p4est_transfer_internal_t * internal)
   P4EST_FREE (internal);
 
   return err;
+}
+
+int
+p4est_transfer_search (p4est_t *p4est, p4est_queries_context_t *c,
+                       p4est_intersect_t intersect_fn)
+{
+  p4est_transfer_internal_t *ti;
+
+  /* Call transfer search begin */
+  ti = p4est_transfer_search_begin (p4est, c, intersect_fn);
+
+  return p4est_transfer_search_end (ti);
 }
 
 int
